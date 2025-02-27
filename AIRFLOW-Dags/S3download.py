0cb25800c156299845897a0a30e176cea6e185db
Sima M.Aminifard <60284799+aminifardsima@users.noreply.github.com>
2025-02-27 10:32:51 +0100

    Update S3download.py

--- AIRFLOW-Dags/S3download.py
+++ AIRFLOW-Dags/S3download.py
@@ -4,18 +4,18 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
 
 def download_s3_file():
-    # Initialize the S3Hook with the Airflow AWS connection ID
+    
     hook = S3Hook(aws_conn_id='con01')
     
-    # Define S3 bucket and file key
+   
     bucket_name = 'sima014498663203'
-    s3_key = 'document/Airflow on helm (3)'  # Path in S3
+    s3_key = 'document/Airflow on helm (3)'  
     
-    # Use /tmp/ as the local download directory (writable in most environments)
+    
     local_file_path = f'/tmp/{s3_key.split("/")[-1]}'
 
     try:
-        # Download file
+        
         hook.get_conn().download_file(bucket_name, s3_key, local_file_path)
         print(f"File '{s3_key}' downloaded successfully to '{local_file_path}'")
     except Exception as e:
