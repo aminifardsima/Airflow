e3142bb65c754bc8868575db1905391607e51f33
Sima M.Aminifard <60284799+aminifardsima@users.noreply.github.com>
2025-02-27 10:34:15 +0100

    Update AWSConTEST.py

--- AIRFLOW-Dags/AWSConTEST.py
+++ AIRFLOW-Dags/AWSConTEST.py
@@ -4,13 +4,13 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
 
 def test_s3_connection():
-    # Initialize the S3Hook with your connection id ('con01')
+    
     hook = S3Hook(aws_conn_id='con01')
-    # Get the underlying boto3 S3 client
+   
     s3_client = hook.get_conn()
-    # Call list_buckets on the boto3 client
+    
     response = s3_client.list_buckets()
-    # Extract bucket names from the response
+   
     buckets = [bucket['Name'] for bucket in response.get('Buckets', [])]
     print("Buckets:", buckets)
     return buckets
